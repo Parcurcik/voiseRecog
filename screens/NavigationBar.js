07cfae54e1b0d92a398d9ab2fcea9b5d9c45f651
Parcurcik <pavelbezborodov@bk.ru>
2022-11-27 12:04:56 +0500

    added logout and train

--- screens/NavigationBar.js
+++ screens/NavigationBar.js
@@ -1,34 +1,86 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
 import Profile from "./Profile";
 import Train from "./Train";
 import Statistics from "./Statistics";
 import { COLORS } from "../assets/colors/colors";
+import  Permission  from "react-native";
+
+
 
 
 const Tab = createBottomTabNavigator()
 
 export default NavigationBar = () => {
     return (
+
             <Tab.Navigator
             screenOptions={{
-                headerShown: false // Потом поменять
+                headerShown: false,
+                tabBarStyle: [
+                    {
+                    height: 80,
+                    position:'absolute',
+                    backgroundColor: 'transparent',
+                    elevation: 0,
+                    display: "flex"
+                    },
+                    null
+                ],
+                tabBarLabelStyle: {color: '#000000'}
+            }}
+            tabBarOptions={{
+                showLabel: false,
             }}>
-                <Tab.Screen name="Profile" component={Profile}/>
-                <Tab.Screen name="Train" component={Train}/>
-                <Tab.Screen name="Statistics" component={Statistics}/>
-            </Tab.Navigator>
+                <Tab.Screen name="Profile" component={Profile} options={{
+                tabBarIcon: ({focused}) => (
+                    <View>
+                        <Image
+                        source={focused ? require('../assets/images/profile_use.png') : require('../assets/images/profile.png') }
+                        style={{
+                            width: 50,
+                            height: 50,
+                        }}
+                        resizeMode='contain'/>
+                    </View>
+                ),
+                }}/>
+                <Tab.Screen name="Train" component={Train} options={{
+                    tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image
+                            source={focused ? require('../assets/images/train_use.png') : require('../assets/images/train.png') }
+                            style={{
+                                width: 50,
+                                height: 50,
+                            }}
+                            resizeMode='contain'/>
+                        </View>
+                    ),
+                }}/>
+                <Tab.Screen name="Statistics" component={Statistics} options={{
+                    tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image
+                            source={focused ? require('../assets/images/statistic_use.png') : require('../assets/images/statistic.png') }
+                            style={{
+                                width: 50,
+                                height: 50,
+                            }}
+                            resizeMode='contain'/>
+                        </View>
+                    ),
+                }}/>
+            </Tab.Navigator>  
     )
 }
 
 const styles = StyleSheet.create({
-    tabMenu: {
-        position: 'absolute',
-        bottom: 25,
-        left: 20,
-        right: 20,
-        backgroundColor: COLORS.green,
-        borderRadius: 15,
-        height: 90
+    img_bg: {
+        width: 412,
+        height: 80
+    },
+
+    img_profile: {
     }
 })
--- screens/Statistics.js
+++ screens/Statistics.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useAuth } from '../AuthContext'
+import Voice from '@react-native-voice/voice';
 
 const Statistics = () => {
   const [user] = useAuth()
+  
   return (
     <View>
       <Text>{user.displayName}</Text>
